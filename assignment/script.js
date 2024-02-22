7cb02b2443fd7f1a61101fd94f0127effd8d4a85
NoamKar <146174873+NoamKar@users.noreply.github.com>
2024-02-22 10:39:15 +0200

    COMMIT

---
+++ assignment/script.js
@@ -0,0 +1,9 @@
+function check(){
+    console.log("1 2 1 2");
+}
+
+function divClicked(){
+    alert("hi im working");
+    document.getElementById("dorothy").style.left = '300px'; 
+    document.getElementById("dorothy").style.top = '400px'; 
+}
